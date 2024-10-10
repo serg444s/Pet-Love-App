421634ffa385229db9d7d8a859e48dad69c0e32f
serg444s <143083342+serg444s@users.noreply.github.com>
2024-10-10 19:15:36 +0000

    Deploying to gh-pages from @ serg444s/Pet-Love-App@02485fe8f26405f60d9cc09a58ab9d949ffc0c89 🚀

---
+++ static/js/482.3578001f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[482],{6482:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"ProfilePage"})}}}]);
+//# sourceMappingURL=482.3578001f.chunk.js.map
