a743e5875968cf3c33609ff961ce6227b322ffe6
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-10-11 10:10:08 +0300

    page 404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,35 +1,31 @@
-import { Link, useNavigate } from "react-router-dom";
-import css from "./NotFoundPage.module.css"
-import { useEffect } from "react";
-import url from "../../assets/img/404.jpg"
+import { Link, useNavigate } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
+import { useEffect } from 'react';
+import url from '../../assets/img/404.png';
 
 const NotFoundPage = () => {
-
   const nav = useNavigate();
   const time = 10000;
 
-  useEffect(() => {
-    const index = setTimeout(() => {
-      nav('/');
-    }, time);
-    return () => clearTimeout(index);
-  }, [nav]);
+  // useEffect(() => {
+  //   const index = setTimeout(() => {
+  //     nav('/');
+  //   }, time);
+  //   return () => clearTimeout(index);
+  // }, [nav]);
 
+  return (
+    <div className={css.container}>
+      <div className={css.wrapper}>
+        <img src={url} alt="not found this page" className={css.img} />
+      </div>
+      <p className={css.text}>Ooops! This page not found :( </p>
 
-return <>
-<p className={css.text}>The page you are looking for may have been moved, deleted,
-or possibly never existed</p>
-<p className={css.text}>You will be returned to the main page in {time / 1000} seconds</p>
-<p className={css.text}>Or just click</p>
-<Link to="/" className={css.link}>
-    Back to main page
-  </Link>
-<div className={css.wrapper}>
-<img src={url} alt="not found this page" className={css.img}/>
-</div>
-
-
-</>
-}
+      <Link to="/" className={css.link}>
+        To home page
+      </Link>
+    </div>
+  );
+};
 
 export default NotFoundPage;
