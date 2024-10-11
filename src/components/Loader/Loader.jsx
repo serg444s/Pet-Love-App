281296a535646ff56064ea59597eafb272b6fc7f
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-10-11 10:17:57 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,26 +1,17 @@
-import { ThreeCircles } from 'react-loader-spinner';
+import { Hourglass } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={styles.overlay}>
-      <ThreeCircles
-        height="300"
-        width="300"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-        wrapperStyle={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        wrapperClass="my-custom-class"
+      <Hourglass
         visible={true}
-        backgroundColor="#FFF4DF"
-        secondaryColor="#F6B83D"
-        strokeWidth={3}
-        animationDuration={800}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={['#F6B83D', '#FFF4DF']}
       />
     </div>
   );
